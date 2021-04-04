36f74deca8b8f05376040ea2c43ae6d67535c1a5
Nikolai Gubarev <BEEFC0DEh@gmail.com>
2021-04-05 01:05:16 +0300

    Add graph output

--- birthdays.cpp
+++ birthdays.cpp
@@ -78,17 +78,72 @@ Probabilities getPs()
     return result;
 }
 
+void drawGraph(const Probabilities &ps, const size_t columns = 70)
+{
+    const float step = float( ps.size() ) / columns;
+    const size_t width = 7;
+    for (size_t row = 0; row <= 10; row++) {
+        using Real = Probabilities::value_type;
+        Real threshold = float(10 - row) / 10.0f;
+        std::cout << std::fixed << std::setprecision(1) << threshold << '|';
+
+        for (size_t column = 0; column < columns; column++) {
+            size_t i = step * column;
+            if ( ps.at(i) >= threshold ) {
+                if (column % width) {
+                    std::cout << '*';
+                } else {
+                    std::cout << '|';
+                }
+
+            } else {
+                std::cout << ' ';
+            }
+        }
+
+        std::cout << '\n';
+    }
+
+    std::cout << "   0^";
+    for (size_t column = width; column < columns; column += width) {
+        std::cout << std::setw(width - 1) << int(column * step) << '^';
+    }
+    std::cout << '\n';
+}
+
 int main(int argc, char *argv[]) {
+    bool graph = false;
+    size_t width = 70;
+
     if (argc > 1) {
         try {
-            int i = std::stoi( argv[1] );
+            for (int arg = 1; arg < argc; arg++) {
+                if ( std::string{ argv[arg] } == "--graph" ) {
+                    graph = true;
+                }
 
-            if (i < 0) {
-                std::cout << "SAMPLES should be >= 0." << '\n';
-                return 0;
+                if ( std::string{ argv[arg] } == "--width" ) {
+                    int i = std::stoi( argv[arg + 1] );
+
+                    if (i < 0) {
+                        std::cout << "--width should be >= 0." << '\n';
+                        return 0;
+                    }
+
+                    width = i;
+                }
+
+                if ( std::string{ argv[arg] } == "--samples" ) {
+                    int i = std::stoi( argv[arg + 1] );
+
+                    if (i < 0) {
+                        std::cout << "--samples should be >= 0." << '\n';
+                        return 0;
+                    }
+
+                    SAMPLES = i;
+                }
             }
-
-            SAMPLES = i;
         }
         catch (std::invalid_argument const &e)
         {
@@ -102,9 +157,16 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    std::cout << "Each test will collect " << SAMPLES << " samples." << '\n';
+
     const auto &ps = getPs();
-    for (size_t i = 0; i < ps.size(); i++) {
-        std::cout << "For " << i << " events the probability is " << std::fixed << std::setprecision(3) << ps.at(i) << '\n';
+
+    if(!graph) {
+        for (size_t i = 0; i < ps.size(); i++) {
+            std::cout << "For " << i << " events the probability is " << std::fixed << std::setprecision(3) << ps.at(i) << '\n';
+        }
+    } else {
+        drawGraph(ps, width);
     }
     return 0;
 }
